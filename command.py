11c850fe3a5939a0bee9fd4ec9a54a62fc3b253c
Fermi Perumal <ferminiveditha@gmail.com>
2022-06-20 14:15:58 -0700

    Add command class to repository

--- command.py
+++ command.py
@@ -1,1 +1,20 @@
+class Command():
+    """
+    Command class that contains the command to be
+    executed and its priority
+    """
+    def __init__(self, command, priority):
+        self.__command = command
+        self.__priority = priority
 
+    def command(self):
+        return self.__command
+
+    def priority(self):
+        return self.__priority
+
+    def setCommand(self, command):
+        self.__command = command
+
+    def setPriority(self, priority):
+        self.__priority = priority
