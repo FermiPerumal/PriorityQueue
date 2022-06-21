a10ce7652a1e51a268ec992cb51ddf359bfa4a43
Fermi Perumal <ferminiveditha@gmail.com>
2022-06-20 20:14:56 -0700

    Add timestamp to command class
    Add timestamp variable and helper methods
    that helps with determining priority for commands
    with same priority

--- command.py
+++ command.py
@@ -1,17 +1,26 @@
+from datetime import datetime
+
 class Command():
     """
     Command class that contains the command to be
-    executed and its priority
+    executed and its priority and timestamp
     """
     def __init__(self, command, priority):
         self.__command = command
         self.__priority = priority
+        self.__timestamp = datetime.now()
 
     def command(self):
         return self.__command
 
     def priority(self):
         return self.__priority
+
+    def timestamp(self):
+        return self.__timestamp
+
+    def isOlderThan(self, other):
+        return self.timestamp() < other.timestamp()
 
     def setCommand(self, command):
         self.__command = command
